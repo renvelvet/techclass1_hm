599009d5e350a43917c5b6a8f7e7755273f504cc
Resha Puspita <reshapuspita734@gmail.com>
2021-07-09 16:44:36 +0900

    rename Player class into Basic_Player

---
+++ Basic_Player.rb
@@ -0,0 +1,30 @@
+class Basic_Player
+	attr_reader :name, :damage
+
+	def initialize(name, hitpoint, damage)
+		@name = name
+		@hitpoint = hitpoint
+		@damage = damage
+	end
+
+	def to_s
+		"#{@name} has #{@hitpoint} hitpoint and #{@damage} attack damage\n"
+	end
+
+	def attack(enemy)
+		puts "#{@name} attacks #{enemy.name} with #{@damage} damage\n"
+		enemy.take_hitpoint(@damage)
+	end
+
+	def take_hitpoint(damage)
+		@hitpoint -= damage
+	end
+
+	def die?
+		if @hitpoint <= 0
+			puts "#{@name} dies\n"
+			true
+		end
+	end
+
+end
